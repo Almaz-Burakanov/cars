3e7c5efefe2e3822ab3f8824221ae73671cee207
Almaz-Burakanov <63657263+Almaz-Burakanov@users.noreply.github.com>
2020-12-13 23:10:34 +0500

    Update 2delet.php

--- 2delet.php
+++ 2delet.php
@@ -4,7 +4,7 @@ $id=$_GET['id_show'];
 mysqli_query($mysql, "DELETE FROM `saloon` WHERE `saloon`.`id_show`='$id'");
 mysqli_query($mysql, "set @autoid :=0");
 mysqli_query($mysql, " update saloon set id_show = @autoid := (@autoid+1)");
-header('Location: ../2app.php');
+header('Location: 2app.php');
 
 
 ?>

d55406f4f070fd23f82273f8cb4952148e9a2f12
Almaz-Burakanov <63657263+Almaz-Burakanov@users.noreply.github.com>
2020-12-14 02:54:32 +0500

    Add files via upload

--- ind.php
+++ ind.php
@@ -3,4 +3,6 @@
     <li><a href="app.php">Автомобили </a>  
     <li><a href="2app.php">Автосалоны </a>  
 	<li><a href="3app.php">Автомобили в наличии </a> 
+	<br><br><button onclick="window.location.href ='p.php';"><img src="http://cdn.onlinewebfonts.com/svg/img_442886.png" width="50" height="50" alt="" style="vertical-align:middle"></button><br><a style="color: red">&emsp;PDF</a>
+	<br><br><button onclick="window.location.href ='x.php';"><img src="https://pced.ru/wp-content/uploads/2020/07/47-3.png" width="50" height="50" alt="" style="vertical-align:middle"></button><br><a style="color: green">&ensp;EXCEL</a>
 </ul>

470195c7d65f5127f9e14feaed6131efacbe39c6
Almaz-Burakanov <63657263+Almaz-Burakanov@users.noreply.github.com>
2020-12-14 00:34:30 +0500

    Update 2delet.php

--- 2delet.php
+++ 2delet.php
@@ -1,6 +1,7 @@
 <?php
 require_once'connect.php';
 $id=$_GET['id_show'];
+mysqli_query($mysql, "DELETE FROM `auto` WHERE `auto`.`id_saloon`='$id'");
 mysqli_query($mysql, "DELETE FROM `saloon` WHERE `saloon`.`id_show`='$id'");
 mysqli_query($mysql, "set @autoid :=0");
 mysqli_query($mysql, " update saloon set id_show = @autoid := (@autoid+1)");

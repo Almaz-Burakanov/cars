e78e0e5743369d2b2f44f49a7afc4ba979f4ea33
Almaz-Burakanov <63657263+Almaz-Burakanov@users.noreply.github.com>
2020-12-14 00:34:46 +0500

    Update delet.php

--- delet.php
+++ delet.php
@@ -1,6 +1,7 @@
 <?php
 require_once'connect.php';
 $id=$_GET['id'];
+mysqli_query($mysql, "DELETE FROM `auto` WHERE `auto`.`id_cars`='$id'");
 mysqli_query($mysql, "DELETE FROM `cars` WHERE `cars`.`id`='$id'");
 mysqli_query($mysql, "set @autoid :=0");
 mysqli_query($mysql, " update cars set id = @autoid := (@autoid+1)");

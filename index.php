9e102e33f4f5670090632459125c35bab5767b71
Almaz-Burakanov <63657263+Almaz-Burakanov@users.noreply.github.com>
2020-12-14 03:19:11 +0500

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 </ul>
 Лаба 5</br>
 <ul id="nav">
-   В РАЗРАБОТКЕ... 
+   ГОТОВО!!!
    <li><a href="ind.php">Автомобили, салоны и автомобильный в наличии</a> 
 </ul>
 <button onclick="window.location.href ='https://github.com/Almaz-Burakanov/cars';"><img src="http://pngimg.com/uploads/github/github_PNG20.png" width="50" height="50" alt="" style="vertical-align:middle"> 
